5df6514732d102787c1e4af19784c82e88be4dd9
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2023-07-27 14:19:51 +0200

    Add: new project - pokedex

--- src/helpers/projectList.ts
+++ src/helpers/projectList.ts
@@ -3,6 +3,11 @@ const projectList = [
     projectLink: 'https://bwiechec.github.io/Twitter-Clone/',
     projectSrc: 'doggter.png',
     projectName: 'Doggter (Twitter-Clone)'
+  },
+  {
+    projectLink: 'https://bwiechec.github.io/PokedexApp/',
+    projectSrc: 'pokedexApp.png',
+    projectName: 'Pokedex App'
   }
 ];
 
