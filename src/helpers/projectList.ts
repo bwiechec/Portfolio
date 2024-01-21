4814d67d624903bd45e4089a5b7444fa793a6844
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-01-21 13:08:02 +0100

    add new project

--- src/helpers/projectList.ts
+++ src/helpers/projectList.ts
@@ -1,16 +1,23 @@
 const projectList = [
   {
+    projectLink: "https://textenger.vercel.app/",
+    githubLink: "https://github.com/bwiechec/Textenger",
+    projectSrc: "textenger.png",
+    projectName: "Textenger **IN PROGRESS**",
+    techStack: ["React", "TypeScript", "Firebase", "Tailwind"],
+  },
+  {
+    projectLink: "https://phone-gear.vercel.app/",
+    githubLink: "https://github.com/bwiechec/phoneGear",
+    projectSrc: "phoneGear.png",
+    projectName: "PhoneGear App",
+    techStack: ["React", "TypeScript", "Firebase"],
+  },
+  {
     projectLink: "https://bwiechec.github.io/Twitter-Clone/",
     githubLink: "https://github.com/bwiechec/Twitter-Clone",
     projectSrc: "doggter.png",
     projectName: "Doggter (Twitter-Clone)",
-    techStack: ["React", "TypeScript"],
-  },
-  {
-    projectLink: "https://bwiechec.github.io/PokedexApp/",
-    githubLink: "https://github.com/bwiechec/PokedexApp",
-    projectSrc: "pokedexApp.png",
-    projectName: "Pokedex App",
     techStack: ["React", "TypeScript"],
   },
   {
@@ -21,11 +28,11 @@ const projectList = [
     techStack: ["React", "TypeScript", "Firebase"],
   },
   {
-    projectLink: "https://phone-gear.vercel.app/",
-    githubLink: "https://github.com/bwiechec/phoneGear",
-    projectSrc: "phoneGear.png",
-    projectName: "PhoneGear App",
-    techStack: ["React", "TypeScript", "Firebase"],
+    projectLink: "https://bwiechec.github.io/PokedexApp/",
+    githubLink: "https://github.com/bwiechec/PokedexApp",
+    projectSrc: "pokedexApp.png",
+    projectName: "Pokedex App",
+    techStack: ["React", "TypeScript"],
   },
 ];
 
