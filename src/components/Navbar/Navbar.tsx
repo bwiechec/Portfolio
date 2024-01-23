7a1dc0ed7c17b65374f3b9c53608027e95292561
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-01-23 21:16:19 +0100

    change to styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,38 +1,31 @@
-import "./navbar.css"
+import "./navbar.css";
 import Socials from "../Socials/Socials";
 
 interface NavbarProps {
   classNameString: string;
 }
 
-const Navbar = ({ classNameString } : NavbarProps) => {
-
+const Navbar = ({ classNameString }: NavbarProps) => {
   const smoothScrollToElement = (selector: string) => {
-
     document.querySelector(selector)?.scrollIntoView({
-      behavior: 'smooth'
+      behavior: "smooth",
     });
-  }
+  };
 
-  return(
-    <nav className={`${classNameString} nav`}>
+  return (
+    <nav className={`${classNameString}`}>
+      <div className=" nav">
+        <Socials />
 
-      <Socials />
-
-      <div className={"nav-sections"}>
-        <button
-          onClick={() => smoothScrollToElement(".about")}
-        >
-          About
-        </button>
-        <button
-          onClick={() => smoothScrollToElement(".projects")}
-        >
-          Projects
-        </button>
+        <div className={"nav-sections"}>
+          <button onClick={() => smoothScrollToElement(".about")}>About</button>
+          <button onClick={() => smoothScrollToElement(".projects")}>
+            Projects
+          </button>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
