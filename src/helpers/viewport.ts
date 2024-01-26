168b5fec84a5bede9ea89bb5d8150c97749fa8ab
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2024-01-26 20:02:45 +0100

    change primary color

--- src/helpers/viewport.ts
+++ src/helpers/viewport.ts
@@ -8,9 +8,9 @@ export const getViewport = () => {
       return "md";
     case width < 1280:
       return "lg";
-    case width < 1536:
+    case width > 1536:
       return "xl";
-    case width <= 1920:
+    case width >= 1920:
       return "2xl";
     default:
       return "lg";
